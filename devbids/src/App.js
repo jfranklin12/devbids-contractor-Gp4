6ff222417fd277affb0baaf93a75496beac26b58
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-29 14:04:20 -0400

    app.js log in routes

--- devbids/src/App.js
+++ devbids/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap";
 import "react-bootstrap";
-import LoginSignup from "./pages/LoginSignup/LoginSignup"
+import LoginSignup from "./pages/LoginSignup/LoginSignup";
 
 function App() {
   return (
-    
-    
+    <div>
+      <div className="routes">
+        <Routes>
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
 
