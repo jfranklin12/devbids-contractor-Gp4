7a53a04fa5dea5e9efe19aa76e073f4970be8bf5
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-29 23:00:37 -0400

    app.js imports

--- devbids/client/src/App.js
+++ devbids/client/src/App.js
@@ -1,4 +1,6 @@
-import logo from "./logo.svg";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 
