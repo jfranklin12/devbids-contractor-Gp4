ff8cf79e26842921671dfd4f93761e7eb2035038
huntersteffner <huntersteffner@gmail.com>
2022-08-30 14:34:17 -0400

    Added styling for create contact form

--- devbids/client/src/components/CreateContract/CreateContract.js
+++ devbids/client/src/components/CreateContract/CreateContract.js
@@ -5,30 +5,40 @@ export default function CreateContract() {
     <div className="form-container">
       <h1>Create New Contract</h1>
       <form className="form-control">
-        <div className="form-section">
-          <h2>Contact Information</h2>
-          <label for="contact-name">Name</label>
-          <input type="text" id="contact-name" name="contact-name" />
-          <label for="phone">Phone</label>
-          <input
-            type="tel"
-            id="phone"
-            name="phone"
-            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
-          />
-          <label for="email">Email</label>
-          <input type="email" id="email" name="email" />
+        <div className="input-section">
+          <div className="form-section">
+            <h2>Contact Information</h2>
+            <label for="contact-name">Name</label>
+            <input type="text" id="contact-name" name="contact-name" />
+            <label for="phone">Phone</label>
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+            />
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="form-section">
+            <h2>Job Information</h2>
+            <label for="title">Job Title</label>
+            <input type="text" id="title" name="title" />
+            <label for="skills">Skills Required</label>
+            <input type="text" />
+            <label for="price">Price Range</label>
+            <input type="number" id="price" name="price" />
+            <label for="completion-date">Needs to be completed by</label>
+            <input type="text" id="completion-date" name="completion-date" />
+          </div>
+          <div className="form-section">
+            <h2>Job Description</h2>
+            <textarea id="description" name="description" />
+          </div>
         </div>
-        <div className="form-section">
-          <h2>Job Information</h2>
-          <label for="title">Job Title</label>
-          <input type="text" id="title" name="title" />
-          <label for='skills'>Skills Required</label>
-          <input type="text" />
-          <label for='description'>Description</label>
-          <textarea id='description' name='description' />
-        </div>
-        <button type="submit">Submit</button>
+        <button className="btn-submit" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   )
