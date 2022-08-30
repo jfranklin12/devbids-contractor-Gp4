b83cf12cd6fd8be1c14e723b5dffdb78fb0f894f
vyang14 <vyang1014888@gmail.com>
2022-08-30 12:13:54 -0400

    models and typeDefs updated

---
+++ devbids/server/models/Comment.js
@@ -0,0 +1,26 @@
+const { Schema, model} = require('mongoose');
+
+const commentSchema = new Schema({
+  username: {
+    type: String,
+    ref: 'User',
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  contractId: {
+    type: Int,
+    ref: 'Contract',
+    required: true
+  },
+  commentDate: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+const Request = model('Request', requestSchema);
+
+module.exports = Request;
