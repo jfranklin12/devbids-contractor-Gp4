17c16cc130e88561373db5050d4f2fed0497ea32
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-31 18:24:43 -0400

    login form

--- devbids/client/src/components/LoginForm/LoginForm.js
+++ devbids/client/src/components/LoginForm/LoginForm.js
@@ -1,10 +1,35 @@
-import React from "react";
-import { Form, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../utils/mutations";
 
 export function LoginForm() {
+  // make state to remember what they type in forms
+  const [userFormData, setUserFormData] = useState({ email: "", password: "" });
+  const [showAlert, setShowAlert] = useState(false);
+
+  // write function that is called when you press login
+  const [login, { error }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
+
   return (
     <Form>
       <div>Welcome Back! Log in below!</div>
+      <Alert
+        dismissible
+        onClose={() => setShowAlert(false)}
+        show={showAlert}
+        variant="danger"
+      >
+        Invalid email or password!
+      </Alert>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
