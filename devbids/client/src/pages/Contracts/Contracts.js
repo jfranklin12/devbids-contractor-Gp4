078ee7cef0c1fab54091e1cb24069d706bc876c0
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-31 15:02:30 -0400

    contract details

--- devbids/client/src/pages/Contracts/Contracts.js
+++ devbids/client/src/pages/Contracts/Contracts.js
@@ -2,10 +2,6 @@ import "./Contracts.css";
 import ContractCard from "../../components/ContractCard/ContractCard";
 import React from "react";
 
-// export default function Contracts() {
-//   return (
-//     <div>
-//       <ContractCard />
-//     </div>
-//   );
-// }
+export default function Contracts() {
+  return <div></div>;
+}
