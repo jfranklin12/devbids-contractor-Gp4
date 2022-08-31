da354909282406e477d30b9fba435611b41af43a
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-31 13:25:09 -0400

    primite contract card styling

--- devbids/client/src/components/ContractCard/ContractCard.js
+++ devbids/client/src/components/ContractCard/ContractCard.js
@@ -5,7 +5,7 @@ function ContractCard() {
   return (
     <Card style={{ width: "50rem" }}>
       <Card.Body>
-        <Card.Title style={{ color: "blue", fontSize: "25px" }}>
+        <Card.Title style={{ color: "#5b57db", fontSize: "25px" }}>
           Job Title: Style Homepage for Theater Company
         </Card.Title>
         <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: "20px" }}>
@@ -17,19 +17,13 @@ function ContractCard() {
         <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: "20px" }}>
           Completion By: 8/31/22
         </Card.Subtitle>
-        <Card.Text style={{ fontSize: "18px" }}>
+        <Card.Text style={{ color: "#5b57db", fontSize: "18px" }}>
           Job Description: This is a blah blah blah blah job that requires blah
           blah blah. We need someone to blah blah blah and update blah blah
           blah. We need an experienced blah blah to work with blha blha blah.
         </Card.Text>
         <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: "18px" }}>
-          Vendor Name: Mary Margaret Taylor
-        </Card.Subtitle>
-        <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: "18px" }}>
-          Phone: 678-678-6788
-        </Card.Subtitle>
-        <Card.Subtitle className="mb-2 text-muted" style={{ fontSize: "18px" }}>
-          Email: taylor.marymargaret@gmail.com
+          Published: mm43678
         </Card.Subtitle>
       </Card.Body>
     </Card>
