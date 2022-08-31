85692e9e6b35db3c4d3396b452d7c150bf992c28
mmtaylor <taylor.marymargaret@gmail.com>
2022-08-31 19:04:27 -0400

    connection/seeds/login connected to backend

--- devbids/client/src/components/LoginForm/LoginForm.js
+++ devbids/client/src/components/LoginForm/LoginForm.js
@@ -3,6 +3,8 @@ import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../../utils/mutations";
 
+import Auth from "../../utils/auth";
+
 export function LoginForm() {
   // make state to remember what they type in forms
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
@@ -19,6 +21,36 @@ export function LoginForm() {
     }
   }, [error]);
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const res = await login({ variables: { ...userFormData } });
+
+      Auth.login(res.data.login.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      email: "",
+      password: "",
+    });
+  };
+
   return (
     <Form>
       <div>Welcome Back! Log in below!</div>
@@ -32,14 +64,28 @@ export function LoginForm() {
       </Alert>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          name="email"
+          onChange={handleInputChange}
+          value={userFormData.email}
+          required
+        />
+        <Form.Control.Feedback type="invalid">
+          Email is required!
+        </Form.Control.Feedback>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button
+        variant="success"
+        disabled={!(userFormData.email && userFormData.password)}
+        type="submit"
+      >
         Log In
       </Button>
     </Form>
