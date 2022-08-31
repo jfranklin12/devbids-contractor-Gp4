ed26169f56cbef620d81dbdfeb9d31e20266d510
vyang14 <vyang1014888@gmail.com>
2022-08-30 23:49:46 -0400

    search bar and about us added to homepage

--- devbids/client/src/components/SearchBar/SearchBar.js
+++ devbids/client/src/components/SearchBar/SearchBar.js
@@ -5,45 +5,47 @@ import './SearchBar.css'
 
 const SearchBar = () => {
 
-    const [userSearch, setUserSearch] = useState([]);
-    const [searchInput, setSearchInput] = useState('');
+    // const [userSearch, setUserSearch] = useState([]);
+    // const [searchInput, setSearchInput] = useState('');
 
-    const [login, { error }] = useMutation([])
-    const handleFormSubmit = async (event) => {
-        event.preventDefault();
+    // const [login, { error }] = useMutation([])
+    // const handleFormSubmit = async (event) => {
+    //     event.preventDefault();
 
-        try {
-            const url  = `localhost`
-            const query = `${url}${searchInput}`
+    //     try {
+    //         const url  = `localhost`
+    //         const query = `${url}${searchInput}`
 
-            const response = await fetch(query)
+    //         const response = await fetch(query)
 
 
-        } catch (err) {
-            console.error(err);
-        }
-    }
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // }
 
 return (
-    <Form onSubmit={handleFormSubmit}>
+    <>
+    <Form onSubmit={{/*handleFormSubmit*/}}>
         <Form.Row>
             <Col xs={12} md={8}>
                 <Form.Control
                     name = 'userSearch'
-                    value = {userSearch}
-                    onChange = {(e) => setUserSearch(e.target.value)}
+                    // value = {userSearch}
+                    // onChange = {(e) => setUserSearch(e.target.value)}
                     type = 'text'
                     size = 'lg'
                     placeholder = 'Example: React'
                     />
             </Col>
             <Col xs={12} md={4}>
-                <Button type='submit' variant='success' size='lg'>
+                <Button type='submit' size='lg'>
                     Search
                 </Button>
             </Col>
         </Form.Row>
     </Form>
+    </>
 );
 };
 
