929d96865b30bf0e6eda8892f5d6b27a46f25c36
mmtaylor <taylor.marymargaret@gmail.com>
2022-09-01 22:12:03 -0400

    profiles render

--- devbids/client/src/pages/Contracts/Contracts.js
+++ devbids/client/src/pages/Contracts/Contracts.js
@@ -1,26 +1,32 @@
-import './Contracts.css'
-import ContractCard from '../../components/ContractCard/ContractCard'
-import {useQuery} from '@apollo/client'
-import React from 'react'
+import "./Contracts.css";
+import ContractCard from "../../components/ContractCard/ContractCard";
+import { useQuery } from "@apollo/client";
+import React from "react";
 
-import {QUERY_CONTRACTS} from '../../utils/queries'
+import { QUERY_CONTRACTS } from "../../utils/queries";
 
 export default function Contracts() {
+  const { loading, data } = useQuery(QUERY_CONTRACTS);
 
-  const {loading, data} = useQuery(QUERY_CONTRACTS)
-
-  const contracts = data?.contracts || []
-  
+  const contracts = data?.contracts || [];
 
   return (
     <div>
-      <div className='contacts-banner'></div>
+      <div className="contacts-banner"></div>
       <div className="form-container">
-        <div onClick={console.log(contracts)} className="contracts-title">Contracts</div>
-        {contracts.map((contract,index) => (
-          <ContractCard title={contract.title} price={contract.price} contractDate={contract.contractDate} description={contract.description} username={contract.username}/>
+        <div onClick={console.log(contracts)} className="contracts-title">
+          Contracts
+        </div>
+        {contracts.map((contract, index) => (
+          <ContractCard
+            title={contract.title}
+            price={contract.price}
+            contractDate={contract.contractDate}
+            description={contract.description}
+            username={contract.username}
+          />
         ))}
       </div>
     </div>
-  )
+  );
 }
--- devbids/client/src/pages/Profile/Profile.js
+++ devbids/client/src/pages/Profile/Profile.js
@@ -2,8 +2,14 @@ import "./Profile.css";
 import React from "react";
 import ContractCard from "../../components/ContractCard/ContractCard";
 import ProfileCard from "../../components/ProfileCard/ProfileCard";
+import { useQuery } from "@apollo/client";
+
+import { QUERY_USER_CONTRACTS } from "../../utils/queries";
 
 export default function Profile() {
+  const { loading, data } = useQuery(QUERY_USER_CONTRACTS);
+
+  const contracts = data?.contracts || [];
   return (
     <div>
       <div className="profile-banner"></div>
@@ -12,6 +18,15 @@ export default function Profile() {
         <div>
           <ProfileCard />
         </div>
+        {contracts.map((contract, index) => (
+          <ContractCard
+            title={contract.title}
+            price={contract.price}
+            contractDate={contract.contractDate}
+            description={contract.description}
+            username={contract.username}
+          />
+        ))}
         <div className="contracts-container">
           <div className="my-contracts">My Contracts</div>
           <div></div>
