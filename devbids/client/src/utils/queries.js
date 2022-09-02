40d4640a1be41aeafe9c8aba6e601a51048dba8f
jfranklin12 <j.franks725@gmail.com>
2022-09-01 20:42:39 -0400

    information added to queries and mutations

--- devbids/client/src/utils/queries.js
+++ devbids/client/src/utils/queries.js
@@ -12,38 +12,58 @@ query User {
 `
 // query for contracts working
 export const QUERY_CONTRACTS = gql`
-query Contracts {
+  query Query {
     contracts {
       username
       title
       description
+      category
       price
       contractDate
+      responses {
+        responseAuthor
+        responseDescription
+        price
+        responseDate
+      }
     }
   }
 `
 // query for contracts by user to render to profile page
 export const QUERY_USER_CONTRACTS = gql`
 query UserContracts {
-    userContracts {
-      username
-      title
-      description
+  userContracts {
+    username
+    title
+    description
+    category
+    price
+    contractDate
+    responses {
+      responseAuthor
+      responseDescription
       price
-      contractDate
+      responseDate
     }
   }
+}
 `
 // query by category Working
 export const QUERY_CATEGORY_CONTRACTS = gql`
   query Category($category: String) {
     category(category: $category) {
       username
+      title
       description
-    title
       category
       price
       contractDate
+      responses {
+        responseAuthor
+        responseDescription
+        price
+        responseDate
+      }
     }
   }
 `
