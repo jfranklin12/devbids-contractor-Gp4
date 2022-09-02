e6ec1fecf866807ccf1fe79d816cbccc77368dc0
vyang14 <vyang1014888@gmail.com>
2022-09-01 22:57:26 -0400

    more changes to CreateContract

--- devbids/client/src/pages/CreateContract/CreateContract.js
+++ devbids/client/src/pages/CreateContract/CreateContract.js
@@ -10,12 +10,11 @@ import './CreateContract.css'
 
 const CreateContract = () =>  {
   const [contractData, setContractData] = useState({
-    contactName: "",
-    email: "",
     title: "",
     category: "",
     price:  "",
-    completionDate: "",
+    description: "",
+    contractDate: "",
   });
 
   const [newContract, { error }] = useMutation(ADD_CONTRACT, {
@@ -28,11 +27,13 @@ const CreateContract = () =>  {
           query: QUERY_CONTRACTS,
           data: { contracts: [newContract, ...contracts] },
         });
+        
       } catch(err) {
         console.error(err);
       }
 
       const { user } = cache.readQuery({ query: QUERY_USER });
+      console.log(user);
       cache.writeQuery({
         query: QUERY_USER,
         data: { user: { ...user, contracts: [...user.contracts, newContract] } },
@@ -57,7 +58,13 @@ const CreateContract = () =>  {
       });
       console.log(data);
       console.log(`^ is data`);
-      setContractData('');
+      setContractData({
+        title: "",
+        category: "",
+        price:  "",
+        description: "",
+        contractDate: "",
+      });
     } catch (err) {
       console.error(err);
     }
@@ -71,21 +78,9 @@ const CreateContract = () =>  {
         <form className="form" onSubmit={handleFormSubmit}>
           <div className="input-section">
             <div className="form-section">
-              <h2>Contact Information</h2>
-              <label for="contact-name">Name:</label>
-              <input type="text" id="contact-name" name="contactName" value="vince" />             
-              <label for="phone">Phone:</label>
-              <input
-                type="tel"
-                id="phone"
-                name="phone"
-                value="555-555-5555"
-              />
-              <label for="email">Email:</label>
-              <input type="email" id="email" name="email" value="vince@yang.com" />
-            </div>
-            <div className="form-section">
               <h2>Job Information</h2>
+              <label for="title">Username:</label>
+              <input type="text" id="username" name="username" value={Auth.getProfile().data.username} disabled={true}/>
               <label for="title">Job Title:</label>
               <input type="text" id="title" name="title" value="do the thing"/>
               <label for="category">Skill Required:</label>
@@ -93,11 +88,11 @@ const CreateContract = () =>  {
               <label for="price" >Price:</label>
               <input className="priceInput" type="number" id="price" name="price" value="500"/>
               <label for="completion-date">Date to be completed by:</label>
-              <input type="date" id="completion-date" name="completionDate" />
+              <input type="date" id="completion-date" name="contractDate" />
             </div>
             <div className="form-section">
               <h2>Job Description</h2>
-              <textarea id="description" name="description" value="do the thing"/>
+              <textarea id="description" name="description" value="really, do the thing"/>
             </div>
           </div>
           <button className="btn-submit" type="submit" onSubmit={handleFormSubmit}>
